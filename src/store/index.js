ef6585e1e4aa4300444b3001e80958c91b9737eb
choisohyun <sohyun4283@naver.com>
2020-09-28 20:59:19 +0900

    feat. id와 비밀번호를 저장하는 store 추가

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    id: "",
+    pw: ""
+  },
+  mutations: {
+    save(state, userInfo) {
+      state.id = userInfo.id;
+      state.pw = userInfo.pw;
+    }
+  }
 });
