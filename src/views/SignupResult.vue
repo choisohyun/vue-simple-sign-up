d9191862ddd9d65bc328ed51488e2eb556cccd36
choisohyun <sohyun4283@naver.com>
2020-09-28 21:01:30 +0900

    feat. 입력했던 id, pw를 store에서 가져와 출력

---
+++ src/views/SignupResult.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="App">
+    <h1>회원가입 결과</h1>
+    <div class="sign-up-form">
+      <p class="sign-up-input">아이디: {{ id }}</p>
+      <p class="sign-up-input">비밀번호: {{ pw }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      id: state => state.id,
+      pw: state => state.pw
+    })
+  }
+};
+</script>
